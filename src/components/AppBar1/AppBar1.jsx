16b04dabd1d185117e87fda708e6d2f23970a07b
Allukaparatha <ahjhrhah3@gmail.com>
2025-08-01 03:06:37 +0500

    Delete All users functionality added

--- src/components/AppBar1/AppBar1.jsx
+++ src/components/AppBar1/AppBar1.jsx
@@ -12,6 +12,12 @@ import { useNavigate } from 'react-router-dom';
 export default function ButtonAppBar() {
     const navigate = useNavigate();
 
+    const AllDelete = () => {
+        if (window.confirm('Are you sure you want to delete all users?')) {
+            alert('You can not delete all users at once. Please delete them one by one.');
+        }
+    };
+
     return (
         <div style={{ margin: '0', padding: '0' }}>
             <Box sx={{ flexGrow: 1 }}>
@@ -21,7 +27,7 @@ export default function ButtonAppBar() {
                             Manage <b>Employees</b>
                         </Typography>
                         <Stack sx={{ marginRight: 4 }} direction="row" spacing={2}>
-                            <Button  variant="contained" color="error" startIcon={<RemoveCircleIcon />}>
+                            <Button onClick={AllDelete}  variant="contained" color="error" startIcon={<RemoveCircleIcon />}>
                                 Delete
                             </Button>
                             <Button onClick={() => {navigate('/adduser')}} variant="contained" color="success" startIcon={<AddCircleIcon />}>
